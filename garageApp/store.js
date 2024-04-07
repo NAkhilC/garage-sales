ebdd5d5f2b4bbdab4603eb76befe06737ad95363
Akhil Nallamothu <42518147+NAkhilC@users.noreply.github.com>
2024-04-07 19:57:42 -0400

    Delete garageApp/store.js

--- garageApp/store.js
+++
@@ -1,9 +0,0 @@
-// store.js
-import {configureStore} from '@reduxjs/toolkit';
-import counterReducer from './store/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    themeMode: counterReducer,
-  },
-});
